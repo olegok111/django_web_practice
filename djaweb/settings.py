93918ddc6ca1ba56112b6976cf999e7e0c6f117d
olegok111 <olegokoyomov2@gmail.com>
2024-06-14 00:49:37 +0300

    Make the site available to local network
    Allow the host machine's local IP address to be served by djaweb

--- djaweb/settings.py
+++ djaweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dwr=p(n6whw*2i#w)*c%_6*bgbe)d-tq(jwlh558kx6opf12sv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.13"]
 
 
 # Application definition
